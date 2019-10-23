ea600df4d20e4b3a8a338db86cae85412ced4818
182088 <Uriel.KANKULUFULUABU@student.umons.ac.be>
2019-10-23 15:02:29 +0200

    Succefull

--- src/main/java/exercise1/Person.java
+++ src/main/java/exercise1/Person.java
@@ -1,4 +1,31 @@
 package exercise1;
 
 class Person {
+	private String name;
+	private int age;
+
+
+	public Person(String name, int age){
+
+			this.name= name;
+			this.age= age;
+
+	}
+	public String getName(){
+
+		return this.name;
+	}
+	public int getAge(){
+
+		return this.age;
+	}
+	public void  Setname(String name){
+
+		this.name=  name;
+	}
+	public void Setage(int age){
+
+		this.age= age;
+	}
+
 }
